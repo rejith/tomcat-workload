928d36cdaf18f63b325f973baf5a7221dfdbb18f
Rejith Krishnan <rejith@usa.net>
2017-09-19 12:24:43 -0400

    Update ExampleTagBase.java

--- webapps/examples/WEB-INF/classes/examples/ExampleTagBase.java
+++ webapps/examples/WEB-INF/classes/examples/ExampleTagBase.java
@@ -16,7 +16,7 @@
 */
 package examples;
 
-import javax.servlet.jsp.JspException2;
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyContent;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 import javax.servlet.jsp.tagext.Tag;
