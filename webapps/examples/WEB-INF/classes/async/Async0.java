9019a0179d49008522990f8907b57db57bf41fcd
Rejith Krishnan <rejith@usa.net>
2017-11-10 21:25:19 -0500

    Update Async0.java

--- webapps/examples/WEB-INF/classes/async/Async0.java
+++ webapps/examples/WEB-INF/classes/async/Async0.java
@@ -27,7 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
-public class Async0 extends HttpServlet {
+public class Async0 extends HttpServletx {
 
     private static final long serialVersionUID = 1L;
 
